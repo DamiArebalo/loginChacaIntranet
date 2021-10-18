6a103c43aaaefbe12f6f2d9a0582897e36a40ad7
DamiArebalo <85137654+DamiArebalo@users.noreply.github.com>
2021-10-18 20:54:16 -0300

    LoginIntranet
    Login de intranet de la escuela chacabuco moron

---
+++ js/backend.js
@@ -0,0 +1,29 @@
+// funciond de validicion
+function validar() {
+	// variables de llamado a los input
+	const pass = document.getElementById('clave').value ;
+	const user = document.getElementById('user').value ;
+
+	if ((user == "") || (pass == "") ) { /*campos vacios*/
+		alert("complete los campos vacios");	
+	}
+	else if (pass.length < 6) { /*contraseña minima*/
+		alert("contraseña minima de 6 caracteres"); 
+		
+
+	}else if (pass.length >= 30){ /*contraseña maxima*/
+		alert("contraseña no puede superar los 30 caracteres");
+		
+	}
+	else if(user.length <4){ /*usuario minimo*/
+		alert("usuario minimo de 4 caracteres");
+		
+	}else{
+		location.href="https://www.tecnica6moron.edu.ar/"
+	}
+}	
+
+
+
+	
+
