00778fd118fafdc16f3d588f508c9277caa78b1f
DamiArebalo <damiiarebalo@gmail.com>
2021-10-21 13:58:49 -0300

    LoginChacaIntranet
    Ajustes del sistema conexion login-registro y registro-login terminado

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,8 @@
+//ESCUCHA DEL FORMULARIO Y LOS INPUTS
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
-const btn = document.getElementById('btn');
+
+//EXPRESIONES REGULARES DE VALIDACION
 const expresiones={
 	nombre: /^[a-zA-ZÁ-ÿ\s]{3,40}$/, //letras y espacios pueden llevar acentos;
 	usuario: /^[a-zA-Z0-9\-\_\.]{4,20}$/, //letras, numeros, guio, guion bajo y punto
@@ -9,6 +11,7 @@ const expresiones={
 	telefono: /^\d{10,14}$/ //10 A 14 NUMEROS
 }
 
+//FUNCION DE VALIDACION
 const validaForm = (e) =>{
 	switch(e.target.name){
 		case"nombre":
@@ -33,6 +36,22 @@ const validaForm = (e) =>{
 	}
 }
 
+//METODO DE ESCUCHA DE INPUTS Y VALIDACION TIEMPO REAL
+inputs.forEach((input)=>{
+	input.addEventListener('keyup', validaForm);//CUANDO LEVANTA UNA TECLA
+	input.addEventListener('blur', validaForm);//CUANDO CLICKEA AFUERA
+});
+
+//OBJETO DE INSTANCIACION DE CAMPOS ERRONEOS
+const campos = {
+	nombre: false,
+	usuario: false,
+	password: false,
+	correo: false,
+	telefono: false 
+}
+
+//METODO DE VALIDACION DE CAMPOS
 const validaCampo = (expresion, input, campo)=>{
 	if(expresion.test(input.value)){
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
@@ -47,9 +66,11 @@ const validaCampo = (expresion, input, campo)=>{
 		document.querySelector(`#grupo__${campo} i`).classList.add('bi-x-circle-fill');
 		document.querySelector(`#grupo__${campo} i`).classList.remove('bi-check-circle-fill');
 		document.querySelector(`#grupo__${campo} .form__input-error`).classList.add('form__input-error-activo');
-		campos[campo]= true;
+		campos[campo]= false;
 	}
 }
+
+//VALIDACION DE CONTRASEÑA 2 
 const  validaPassword2 = () =>{
 	const inputpassword1 = document.getElementById('password');
 	const inputpassword2 = document.getElementById('password2');
@@ -70,31 +91,20 @@ const  validaPassword2 = () =>{
 		campos[password]=true;
 	}
 }
-inputs.forEach((input)=>{
-	input.addEventListener('keyup', validaForm)
-	input.addEventListener('blur', validaForm)
-});
 
-const campos = {
-	nombre: false,
-	usuario: false,
-	password: false,
-	correo: false,
-	telefono: false 
-}
-	
-btn.addEventListener('submit', (e) => {
+
+///FUNCION DE VALIDACION DEL BOTON SUBMIT
+formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
-	if(campos.nombre  && campos.usuario && campos.password && campos.pasword2 && campos.correo && campos.telefono){
-	
-		console.logo('no funca');
-		document.getElementById('form__exito').classList.remove('form__exito-activo');
-		// setTimeout(()=>{
-			
-		// document.getElementById('form__exito').classList.remove('form__exito-activo');
-		// }, 5000);
-	
+	formulario.reset();
+	if(campos.nombre  && campos.usuario && campos.password && campos.correo && campos.telefono){
+		document.getElementById('form__mensaje').classList.remove('form__mensaje-activo');
+
+		window.location.href='login.html';
+		
+	}else{
+		document.getElementById('form__mensaje').classList.add('form__mensaje-activo');
 	}
-})
+});
 
 
--- js/validaLogin.js
+++ js/validaLogin.js
@@ -1,26 +1,74 @@
-// funcion de validicion
-function validar() {
-	// variables de llamado a los input
-	const pass = document.getElementById('clave').value ;
-	const user = document.getElementById('user').value ;
 
-	if ((user == "") || (pass == "") ) { /*campos vacios*/
-		alert("complete los campos vacios");	
-	}
-	else if (pass.length < 6) { /*contraseña minima*/
-		alert("contraseña minima de 6 caracteres"); 
-		
+//ESCUCHA DE FORMULARIO Y DE LOS INPUTS
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
 
-	}else if (pass.length >= 30){ /*contraseña maxima*/
-		alert("contraseña no puede superar los 30 caracteres");
+// EXPRESIONES REGULARES PARA VALIDACION
+const expresiones={
+	usuario: /^[a-zA-Z0-9\-\_\.]{4,20}$/, //letras, numeros, guion, guion bajo y punto
+	password: /^.{4,20}$/, // todo de 4 a 20 digitos
+
+}
+
+
+
+// FUNCION DE VALIDACION
+const validaForm = (e) =>{
+	switch(e.target.name){
+		case"user":
+			validaCampo(expresiones.usuario, e.target, 'user');
+		break;
+		case"password":
+		validaCampo(expresiones.password, e.target, 'password');
+		break;
 		
-	}
-	else if(user.length <4){ /*usuario minimo*/
-		alert("usuario minimo de 4 caracteres");
-		
-	}else{
-		btn.addEventListener(('submit'), (e) => {
-			location.href="https://www.tecnica6moron.edu.ar/";
-		})
+	
 	}
 }
+
+// VALIDAR CADA INPUT CADA VEZ QEU SUELTEN UNA TECAL O HAGAN CLICK AFUER
+inputs.forEach((input)=>{
+	input.addEventListener('keyup', validaForm); //TECLA LEVANTADA
+	input.addEventListener('blur', validaForm); //CLICK AFUERA
+});
+
+// INSTANCIACION DE CAMPOS ERRONEOS
+const campos = {
+	user: false,
+	password: false
+}
+
+
+// METODO PARA VALIDACION Y AGREGACION DE CLASES DE ESTILOS
+const validaCampo = (expresion, input, campo)=>{
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('bi-x-circle-fill');
+		document.querySelector(`#grupo__${campo} i`).classList.add('bi-check-circle-fill');
+		document.querySelector(`#grupo__${campo} .form__input-error`).classList.remove('form__input-error-activo');
+		campos[campo] = true;
+	}else{
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('bi-x-circle-fill');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('bi-check-circle-fill');
+		document.querySelector(`#grupo__${campo} .form__input-error`).classList.add('form__input-error-activo');
+		campos[campo]= false;
+	}
+}
+
+
+// CODIGO DE BOTON CUANDO SE APRETA
+
+formulario.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	formulario.reset();
+	if(campos.user && campos.password){//SI LOS CAMPOS SON VERDADEROS...
+		document.getElementById('form__mensaje').classList.remove('form__mensaje-activo');
+		window.location.href ='https://www.tecnica6moron.edu.ar/';
+
+	}else{//SI NO SON VERDADEROS...
+		document.getElementById('form__mensaje').classList.add('form__mensaje-activo');
+	}
+})
